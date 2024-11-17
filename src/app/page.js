88457c81025b98c8ab0945724b47f1f023786fc0
Nemanja Mijailovic <nemanja.mijailovic2912@gmail.com>
2024-11-17 04:49:44 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
-import HomeBody from "./home/home/home";
+"use client";
+
+import Loader from "@/components/loader/loader";
+import Hero from "./home/hero/hero";
+import About from "./home/about/about";
+import Gallery from "./home/gallery/gallery";
+import Work from "./home/work/work";
+import Contact from "./home/contact/contact";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [load, setLoad] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(true);
+    }, [50]);
+  }, []);
+
   return (
     <div>
-      <HomeBody />
+      <Loader />
+      <Hero />
+      {load && <About />}
+      <Gallery />
+      <Work />
+      <Contact />
     </div>
   );
 }
