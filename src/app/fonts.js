f37c70c26ab1b590f488d6e1d2c6e5630ce80d39
Nemanja Mijailovic <nemanja.mijailovic2912@gmail.com>
2024-11-21 07:04:06 +0100

    .

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,14 +1,7 @@
-import { Rubik, Rubik } from "next/font/google";
+import { Rubik } from "next/font/google";
 
 export const rubik = Rubik({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-rubik",
-});
-
-export const sourGummy = Rubik({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-sourGummy",
-  weight: ["400", "700", "800"],
 });
